b2b4d8b164e090aec69170e1abdc203f93be636c
S. Bartlett <stuart.bartlett@diamond.ac.uk>
2021-11-30 16:02:43 +0000

    added d6 - and changed pV to the select boxes for stripe and d6

--- high_low_nrg_change.py
+++ high_low_nrg_change.py
@@ -11,11 +11,11 @@ only uses autofeedback option & Energy (compound) from
 import epics
 from time import sleep
 
-def findPitch(energy):
-    return m*energy+c #linear
+#def findPitch(energy):
+    #return m*energy+c #linear
 
-def findRoll(energy):
-    return m*energy+c #linear
+#def findRoll(energy):
+    #return m*energy+c #linear
 
 
 #### USER Define ####
@@ -35,19 +35,32 @@ rollNotMoving = epics.PV('BL18I-MO-DCM-01:XTAL2:ROLL.DMOV')
 pitchNotMoving = epics.PV('BL18I-MO-DCM-01:XTAL2:PITCH.DMOV')
 braggNotMoving = epics.PV('BL18I-MO-DCM-01:BRAGG.DMOV')
 stripeNotMoving = epics.PV('BL18I-OP-HRM-01:MPY:DMOV')
-
+filter_D6_NotMoving = epics.PV('BL18I-DI-PHDGN-06:A:MP:DMOV')
 fb_x = epics.PV('BL18I-MO-DCM-01:XTAL2:ROLL:FB.FBON')
 fb_y = epics.PV('BL18I-MO-DCM-01:XTAL2:PITCH:FB.FBON')
 fb_x_auto = epics.PV('BL18I-MO-DCM-01:XTAL2:ROLL:FB:AUTO')
 fb_y_auto = epics.PV('BL18I-MO-DCM-01:XTAL2:PITCH:FB:AUTO')
 
-fb_x_auto.put(1)
+fb_x_auto.put(1) 
 fb_y_auto.put(1)
 
 dcmEnergyTarget = epics.PV('BL18I-MO-DCM-01:ENERGY')
 dcmEnergyRBV = epics.PV('BL18I-MO-DCM-01:ENERGY.RBV')
-coating = epics.PV('BL18I-OP-HRM-01:Y.VAL') # change of Stripe on HRM
-#filter move ?
+coating = epics.PV('BL18I-OP-HRM-01:MPY:SELECT') # Stripe on HRM
+filter_D6 = epics.PV('BL18I-DI-PHDGN-06:A:MP:SELECT') # d6 filter
+
+
+
+
+while True:
+    if filter_D6_NotMoving.get() != 1:
+        print('moving')
+        sleep(1)
+
+#filter_D6.put('gap') # no filter
+#filter_D6.put('15 m Al') # 15 m Al filter
+
+#if braggNotMoving.get() != 1 and rollNotMoving.get() != 1 and pitchNotMoving.get() and != 1 stripeNotMoving.get() and != 1 filter_D6_NotMoving.get() != 1:
 
 #### Changes associated with Energy changes   ######
 
@@ -62,8 +75,13 @@ while True:
             fb_y.put(0) 
             PitchSET.put(findPitch(dcmEnergyTarget.get()))
             RollSET.put(findRoll(dcmEnergyTarget.get()))
-            coating.put(19.2) # Si stripe value
-            if braggNotMoving.get() and rollNotMoving.get() and pitchNotMoving.get() and stripeNotMoving.get():
+            coating.put('Silicon(no coat') # Si stripe value
+            filter_D6.put('gap')
+            while moving == 0:
+                if braggNotMoving.get() != 1 or rollNotMoving.get() != 1 or pitchNotMoving.get() != 1 or stripeNotMoving.get() != 1 or filter_D6_NotMoving.get() != 1:
+                    moving = 0
+                else:
+                    
                 print('PASSING CONTROL TO THE AUTO FEEDBACK')
                 fb_x_auto.put(1)
                 fb_y_auto.put(1)
@@ -76,11 +94,13 @@ while True:
             fb_y.put(0) 
             PitchSET.put(findPitch(dcmEnergyTarget.get()))
             RollSET.put(findRoll(dcmEnergyTarget.get()))
-            coating.put('XXX') # Rh stripe value
+            coating.put(19.2) # Rh stripe value
             if braggNotMoving.get() and rollNotMoving.get() and pitchNotMoving.get() and stripeNotMoving.get():
                 print('PASSING CONTROL TO THE AUTO FEEDBACK')
                 fb_x_auto.put(1)
                 fb_y_auto.put(1)
     else:
         sleep(0.1)
-            
+'''
+
+
